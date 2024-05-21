e35c0065bdb1bbe62511c94b2ea3f12ec95873be
Lkanath Panda <lokanthpanda128@gmai.com>
2024-05-21 19:52:41 +0530

    Project Section Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Experience from "@/components/Experience";
+// import Experience from "@/components/Experience";
 import HeroSection from "@/components/HeroSection";
+import Projects from "@/components/Projects";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
      <HeroSection/>
-     <Experience/>
+     {/* <Experience/> */}
+     <Projects/>
     </main>
   );
 }
