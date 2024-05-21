ab75724e882d7593122f21dd8546541479858593
Lkanath Panda <lokanthpanda128@gmai.com>
2024-05-21 23:20:30 +0530

    experience page added for the deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// import Experience from "@/components/Experience";
+import Experience from "@/components/Experience";
 import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
 import Projects from "@/components/Projects";
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
      <HeroSection/>
-     {/* <Experience/> */}
+     <Experience/>
      <Projects/>
      <SkillSection/>
      <AboutSection/>
