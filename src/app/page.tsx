ab0c93d354ee0ef8a448ae3a60f6d082faded4ac
Lkanath Panda <lokanthpanda128@gmai.com>
2024-05-21 20:00:24 +0530

    Skill Section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import Experience from "@/components/Experience";
 import HeroSection from "@/components/HeroSection";
 import Projects from "@/components/Projects";
+import SkillSection from "@/components/SkillSection";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
      <HeroSection/>
      {/* <Experience/> */}
      <Projects/>
+     <SkillSection/>
     </main>
   );
 }
