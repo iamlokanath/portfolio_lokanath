1ed4e6e15315ba5c76f3219cb9a03869f1e38b5d
Lkanath Panda <lokanthpanda128@gmai.com>
2024-05-21 23:13:06 +0530

    Contact Page Created

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+"use client";
+import React, { FormEvent, useState } from 'react';
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
+
 
 function page() {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+  
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      alert('Submitted:', { email, message });
+    };
+
+
   return (
-    <div>
-      Contact
+    <div  className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
+
+      <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
+
+      <div className="max-w-2xl mx-auto p-4 relative z-10">
+        {' '}
+        {/* Add relative and z-10 to bring content to the front */}
+        <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
+          Contact Me
+        </h1>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde qui culpa quod aperiam voluptatibus illo, architecto sequi repellendus saepe impedit, sed, explicabo similique adipisci rem minima. Impedit sunt quis animi.
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-4 mt-4">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Your email address"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
+            required
+          />
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Your message"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
+            rows={5}
+            required
+          ></textarea>
+          <button
+            type="submit"
+            className="px-6 py-2 rounded-lg bg-teal-500 text-white font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+
+
     </div>
   )
 }
